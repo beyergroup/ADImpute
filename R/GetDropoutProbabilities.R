550559adb4237ba21641f62d77cae3e4ffe89cdf
adesous1 <anacarolinaleote@gmail.com>
2020-06-03 17:27:57 +0200

    Add vignettes and License

--- R/GetDropoutProbabilities.R
+++ R/GetDropoutProbabilities.R
@@ -1,7 +1,27 @@
+# ADImpute predicts unmeasured gene expression values from single cell
+# RNA-sequencing data (dropout imputation). This R-package combines multiple
+# dropout imputation methods, including a novel gene regulatory network-based
+# method.
+# Copyright (C) 2020  Ana Carolina Leote
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # code from scImpute ----------------------------------------------------------
 
 read_count <-
-  function (filetype, path, out_dir, type, genelen) 
+  function (filetype, path, out_dir, type, genelen)
   {
     if(filetype == "csv") {
       raw_count = read.csv(path, header = TRUE, row.names = 1)
@@ -16,12 +36,12 @@ read_count <-
     raw_count = as.matrix(raw_count)
     print(paste("number of genes in raw count matrix", nrow(raw_count)))
     print(paste("number of cells in raw count matrix", ncol(raw_count)))
-    
+
     if(type == "TPM"){
       if(length(genelen) != nrow(raw_count)) stop("number of genes in 'genelen' and count matrix do not match! ")
       raw_count = sweep(raw_count, 1, genelen, FUN = "*")
     }
-    
+
     totalCounts_by_cell = colSums(raw_count)
     saveRDS(totalCounts_by_cell, file = paste0(out_dir, "totalCounts_by_cell.rds"))
     totalCounts_by_cell[totalCounts_by_cell == 0] = 1
@@ -43,7 +63,7 @@ find_va_genes = function(parslist, subcount){
   mu = parslist[, "mu"]
   sgene1 = which(mu <= log10(1+1.01))
   # sgene2 = which(mu <= log10(10+1.01) & mu - parslist[,5] > log10(1.01))
-  
+
   dcheck1 = dgamma(mu+1, shape = parslist[, "alpha"], rate = parslist[, "beta"])
   dcheck2 = dnorm(mu+1, mean = parslist[, "mu"], sd = parslist[, "sigma"])
   sgene3 = which(dcheck1 >= dcheck2 & mu <= 1)
@@ -54,7 +74,7 @@ find_va_genes = function(parslist, subcount){
 
 
 calculate_weight <-
-  function (x, paramt) 
+  function (x, paramt)
   {
     pz1 = paramt[1] * dgamma(x, shape = paramt[2], rate = paramt[3])
     pz2 = (1 - paramt[1]) * dnorm(x, mean = paramt[4], sd = paramt[5])
@@ -67,26 +87,26 @@ calculate_weight <-
 
 
 GetDropoutProbabilities <- function(infile, count_path, out_dir, type, genelen, drop_thre, data){
-  
-  count <- as.matrix(read_count(filetype = infile, path = count_path, out_dir = out_dir, 
+
+  count <- as.matrix(read_count(filetype = infile, path = count_path, out_dir = out_dir,
                                 type = type, genelen = genelen))
-  
+
   clust <- readRDS(paste0(out_dir, "clust.rds"))
-  
+
   drop_probs <- list()
-  
+
   for(cc in 1:max(clust, na.rm = T)){
-    
+
     # code from scImpute ------------------------------------------------------
     cells = which(clust == cc)
-    
+
     parslist = readRDS(paste0(out_dir, "pars", cc, ".rds"))
-    
+
     print("searching for valid genes ...")
     valid_genes = find_va_genes(parslist, subcount = count[, cells])
     subcount = count[valid_genes, cells, drop = FALSE]
     parslist = parslist[valid_genes, , drop = FALSE]
-    
+
     droprate = t(sapply(1:length(valid_genes), function(i) {
       wt = calculate_weight(subcount[i, ], parslist[i, ])
       return(wt[, 1])
@@ -94,25 +114,25 @@ GetDropoutProbabilities <- function(infile, count_path, out_dir, type, genelen, 
     mucheck = sweep(subcount, MARGIN = 1, parslist[, "mu"], FUN = ">")
     droprate[mucheck & droprate > drop_thre] = 0
     # -------------------------------------------------------------------------
-    
+
     to.out <- droprate
     rownames(to.out) <- rownames(subcount)
     drop_probs[[cc]] <- to.out
   }
-  
+
   droprob_mat <- matrix(data = NA, nrow = nrow(data), ncol = ncol(data),
                         dimnames = dimnames(data))
   for(r in drop_probs){
     droprob_mat[rownames(r),colnames(r)] <- r
   }
   saveRDS(droprob_mat, "scImpute_dropout_probabilities.rds")
-  
+
   return(droprob_mat)
 }
 
 
 SetBiologicalZeros <- function(imputation, drop_probs, thre, was_zero){
-  
+
   if(!all.equal(dim(imputation), dim(drop_probs))){
     # limit both to the set of common genes / cells
     common_genes <- intersect(rownames(imputation), rownames(drop_probs))
@@ -120,14 +140,14 @@ SetBiologicalZeros <- function(imputation, drop_probs, thre, was_zero){
     imputation <- imputation[common_genes, common_cells]
     drop_probs <- drop_probs[common_genes, common_cells]
   }
-  
+
   # do not impute entries with NA probability (give prob = 0)
   drop_probs[is.na(drop_probs)] <- 0
-  
+
   # set prob < threshold to zero
   set_to_zero <- drop_probs < thre
   imputed_entries <- was_zero & (imputation != 0) # limit to imputed values
   imputation[set_to_zero & imputed_entries] <- 0
-  
+
   return(imputation)
 }
--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 ADImpute predicts unmeasured gene expression values from single cell RNA-sequencing data (dropout imputation). This R-package combines multiple dropout imputation methods, including a novel gene regulatory network-based method.
 
 ## Getting started
-The following R commands should allow you to install ADImpute from github:
+ADImpute requires R version 3.4.4 and the R packages scImpute, SCRABBLE and DrImpute to be installed.
+The following R commands should allow you to install ADImpute from github (< 10 seconds):
 
 ```
 # install.packages("devtools")
 devtools::install_github("anacarolinaleote/ADImpute")
 library(ADImpute)
 ```
+
+For an example of the usage of the ADImpute package, follow the 20-minute [tutorial](https://aanacarolinaleote/ADImpute/vignettes/ADImpute_tutorial.html).
+
+ADImpute was developed and tested in R 3.4.4 on Fedora release 27.
+
+For further questions, please contact: ana.carolina.leote@uni-koeln.de
